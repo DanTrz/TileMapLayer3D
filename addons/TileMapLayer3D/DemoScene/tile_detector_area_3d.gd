c8fa403e4ed83821c463585e073d5114358e46a5
DanTrz <danielmtp@hotmail.com>
2026-05-20 14:30:14 +0100

    demo scene updates

--- addons/TileMapLayer3D/DemoScene/tile_detector_area_3d.gd
+++ addons/TileMapLayer3D/DemoScene/tile_detector_area_3d.gd
@@ -4,24 +4,49 @@ extends Area3D
 
 @onready var start_point_marker_3d: Marker3D = $StartPointMarker3D
 @export var generate_collision: bool = false
+@onready var door_action_label: Label3D = $DoorActionLabel
+
 
 var is_door_open:bool = false
+var can_open_door:bool = false
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	self.body_entered.connect(on_body_entered)
+	self.body_exited.connect(on_body_exited)
+
+	can_open_door = false
+	door_action_label.visible = false
+
+func _unhandled_input(event: InputEvent) -> void:
+	if not (event is InputEventKey and event.pressed and not event.echo):
+		return
+	match event.keycode:
+		KEY_F1:open_close_door()
+		# KEY_F2: _erase_around_player()
+
+func open_close_door() -> void:
+	if not can_open_door:
+		return
+	var tile_info: PlacedTileInfo = get_tile_info()
+	if tile_info and tile_map_layer_3d:
+		tile_map_layer_3d.runtime_api.swap_tile_collection_texture(tile_info, true, 2, 0.15)
+		if generate_collision:
+			tile_map_layer_3d.runtime_api.set_collision_for_region(tile_info, true, true)
+	is_door_open = true
 
 func on_body_entered(body: Node3D) -> void:
 	# print("on_body_entered - Called")
 	if body is TestPlayer:
-		# print("body - Is TestPlayer: ", body.name)
-		var tile_info: PlacedTileInfo = get_tile_info()
-		# print("Original Tile => tile_info key: ", tile_info.tile_key , " Grid Pos: ", tile_info.grid_position, " Orientation: ", tile_info.orientation)
-		if tile_info and tile_map_layer_3d:
-			# tile_map_layer_3d.runtime_api.swap_tile_collection_texture(tile_info, true)
-			tile_map_layer_3d.runtime_api.swap_tile_collection_texture(tile_info, true, 2, 0.15)
-			if generate_collision:
-				tile_map_layer_3d.runtime_api.set_collision_for_region(tile_info, true, true)
+		can_open_door = true
+		door_action_label.visible = true
+
+func on_body_exited(body: Node3D) -> void:
+	# print("on_body_exited - Called")
+	if body is TestPlayer:
+		can_open_door = false
+		door_action_label.visible = false
 
 func get_tile_info() -> PlacedTileInfo:
 	if not tile_map_layer_3d:
